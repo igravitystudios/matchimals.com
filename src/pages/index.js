a05dd73d9511827d0fc35393666abb058107920d
Chris Heninger <chris@igravitystudios.com>
2018-05-29 15:08:39 -0700

    🎨

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,15 @@ import SEO from "../seo";
 export default () => (
   <div className="container">
     <SEO />
-    <img src="Icon-App-1024x1024.png" className="app-icon" />
+    <img src="/images/logo.png" className="app-icon" />
     <h1>Matchimals</h1>
     <div className="coming-soon">Coming Soon</div>
     <div>
-      <img src="badge-app-store.svg" className="download-icon" />
-      <img src="badge-google-play.svg" className="download-icon" />
+      <img src="/images/badge-app-store.svg" className="download-icon" />
+      <img src="/images/badge-google-play.svg" className="download-icon" />
     </div>
+    <a href="https://www.matchimals.fun/" className="play-now">
+      Play the original here!
+    </a>
   </div>
 );
